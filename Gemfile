7f46339dcfc8674b2c72419d408362329529d803
yuralala8 <yc1435@nyu.edu>
2017-07-20 21:55:25 -0400

    argggs

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'sinatra', require: 'sinatra/base'
 gem 'sinatra-reloader', require: false
 gem 'shotgun'
 gem 'pry'
+gem 'byebug'
 gem 'tux'
 gem 'require_all'
 
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+
+    attr_accessor :name, :weight, :height
+
+    PIRATES = []
+
+    def initialize(args)
+        @name = args[:name]
+        @weight = args[:weight]
+        @height = args[:height]
+        PIRATES << self
+    end
+
+    def self.all
+        PIRATES
+    end
+
 end
